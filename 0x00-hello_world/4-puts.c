64ae1b54aeb7a83bd354191d1bc9190aa1f40134
Anass <anaschtioui65@gmail.com>
2023-10-05 15:34:46 -0700

    assembler

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Use puts instead of printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void) 
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+	puts(str);
+	return (0); 
+}
