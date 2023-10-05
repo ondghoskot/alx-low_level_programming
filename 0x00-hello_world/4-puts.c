66fdf11fe35565ccd40196c92a58ab231fafed75
Anass <anaschtioui65@gmail.com>
2023-10-05 15:47:27 -0700

    First C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void) 
+int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
-	return (0); 
+	return (0);
 }
