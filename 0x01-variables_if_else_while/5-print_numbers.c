fd115421eca069d923a146143154645cf62ec6b2
Anass <anaschtioui65@gmail.com>
2023-10-06 12:25:23 -0700

    task done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sdn = 0;
+
+	while (sdn < 10)
+	{
+		printf("%d", sdn);
+		sdn++;
+	}
+	printf("\n");
+	return (0);
+}
