b65ae49129864f803a54fa6e06437d6de210c22a
Anass <anaschtioui65@gmail.com>
2023-10-06 12:05:02 -0700

    task done

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int ch = 'a';
+
+  while (ch <= 'z')
+  {
+      putchar(ch);
+      if (ch == 'd' || ch == 'p')
+      {
+	  ch += 2;
+      }
+      else
+      {
+	  ch++;
+      }
+  }
+  putchar('\n');
+  return (0);
+}
