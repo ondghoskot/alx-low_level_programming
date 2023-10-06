f175cac616602cb5bbc967b8112d9222d557176a
Anass <anaschtioui65@gmail.com>
2023-10-06 12:33:07 -0700

    task done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sdn = 48;
+
+	while (sdn <= 57)
+	{
+		putchar(sdn);
+		sdn++;
+	}
+	putchar('\n');
+	return (0);
+}
