357dd67726b984be3de76a37a3f45ba215ecda98
Anass <anaschtioui65@gmail.com>
2023-10-06 19:04:20 -0700

    advanced task done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d1 = 48;
+	int d2 = 49;
+	int d3 = 50;
+
+	while (d1 <= 57)
+	{
+		while (d2 <= 57)
+		{
+			while (d3 <= 57)
+			{
+				putchar(d1);
+				putchar(d2);
+				putchar(d3);
+				if (!(d1 == 55 && d2 == 56 && d3 == 57))
+				{
+					putchar(44);
+					putchar(32);
+				}
+				d3++;
+			}
+			d2++;
+			d3 = 51;
+			while (d1 == d2 == d3 || d1 > d2 > d3)
+			{
+				d3++;
+			}
+		}
+		d1++;
+	}
+	putchar('\n');
+	return (0);
+}
