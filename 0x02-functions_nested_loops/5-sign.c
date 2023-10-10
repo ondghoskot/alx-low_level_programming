fbfb6c3a2f88917b1fb05847890fd3deb3e5d847
Anass <anaschtioui65@gmail.com>
2023-10-10 13:33:06 -0700

    task done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <unistd>
+
+/**
+ * print_sign - prints sign of input argument
+ *
+ * @n: input argument, an integer
+ *
+ * Return: 1 if n > 0, 0 if n == 0, and -1 if n  < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
