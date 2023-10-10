267b05fc2d60abdcf16d98a6e57f35268d4b4e0e
Anass <anaschtioui65@gmail.com>
2023-10-10 13:57:25 -0700

    task done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of argument
+ *
+ * Return: absolute value (positive)
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a *= -1;
+	}
+	return (a);
+}
