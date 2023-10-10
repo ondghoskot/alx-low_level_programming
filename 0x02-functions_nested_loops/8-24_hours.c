67688e71e48d0b4c56cbd051b9a24888107d254e
Anass <anaschtioui65@gmail.com>
2023-10-10 15:41:32 -0700

    task done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int i = '0';
+
+	while (i <= '2')
+	{
+		int j = '0';
+
+		while (j <= '3')
+		{
+			int k = '0';
+
+			while (k <= '5')
+			{
+				int l = '0';
+
+				while (l <= '9')
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(' ');
+					_putchar(':');
+					_putchar(' ');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
