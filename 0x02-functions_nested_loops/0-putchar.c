723ee5f5e711d22d68a00761580091fd92c8ff4b
Anass <anaschtioui65@gmail.com>
2023-10-10 06:20:44 -0700

    task done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,13 @@
 #include <unistd.h>
-#include "main.h"
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
 int main(void)
 {
-	const char *str = "_putchar";
-	int i;
-
-	while (str[i] != '\0') 
-	{
-		_putchar(str[i]);
-		i++;
-	}
+	const char *str = "_putchar\n";
+	write(1, str, sizeof(str) + 1);
 	return (0);
 }
