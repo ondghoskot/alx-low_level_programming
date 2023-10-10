202dc83cffbae4d4b885dd3f7a6abc637685bf83
Anass <anaschtioui65@gmail.com>
2023-10-10 16:22:23 -0700

    task done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,39 +5,16 @@
  */
 void jack_bauer(void)
 {
-	int i = '0';
-
-	while (i <= '2')
+	for (int i = 0; i <= 23; i++)
 	{
-		int j = '0';
-
-		while (j <= '9')
+		for (int j = 0; j <= 59; j++)
 		{
-			int k = '0';
-
-			while (k <= '5')
-			{
-				int l = '0';
-
-				while (l <= '9')
-				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(':');
-					_putchar(k);
-					_putchar(l);
-					_putchar('\n');
-					l++;
-				}
-				k++;
-				if (i == '2' && j == '3' && k == '5' && l == '9')
-				{
-					goto end;
-				}
-			}
-			j++;
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(':');
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			_putchar('\n');
 		}
-		i++;
 	}
-	end:
 }
