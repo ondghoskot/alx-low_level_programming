3b28e676200118718edaf0583a4e96587a105c6f
Anass <anaschtioui65@gmail.com>
2023-10-11 15:12:35 -0700

    task done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,19 +22,19 @@ void print_times_table(int n)
 				_putchar('0' + result);
 				_putchar(',');
 				_putchar(' ');
-				if (result < 10)
+				if (result < 10 && column != 0)
 				{
 					_putchar(' ');
 					_putchar(' ');
 					_putchar('0' + result);
 				}
-				else if (result < 100)
+				else if (result < 100 && column != 0)
 				{
 					_putchar(' ');
 					_putchar('0' + ((result / 10) % 10));
 					_putchar('0' + result % 10);
 				}
-				else
+				else if (result >= 100 && column != 0)
 				{
 					_putchar('0' + result / 100);
 					_putchar('0' + ((result / 10) % 10));
