8154622eff08c67366d039ba01df82aca1f73f70
Anass <anaschtioui65@gmail.com>
2023-10-12 12:54:46 -0700

    task done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		int j = 0;
+
+		while (j <= 14)
+		{
+			if (j < 10)
+			{
+				_putchar('0' + j);
+			}
+			else
+			{
+				_putchar('0' + (j / 10));
+				_putchar('0' + (j % 10));
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
