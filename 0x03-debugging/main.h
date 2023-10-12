2e1d13538becd45e7f87b715ccfd8171277b9eb7
Anass <anaschtioui65@gmail.com>
2023-10-12 04:58:21 -0700

    task done

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -15,5 +15,6 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
 
 #endif
