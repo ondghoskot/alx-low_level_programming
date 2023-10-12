cceaafe94241e1f31ebc432bbc8454d65f0a94b5
Anass <anaschtioui65@gmail.com>
2023-10-12 12:18:50 -0700

    task done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - takes in 2 integers and multiplies them together
+ * @a: first input integer
+ * @b: second input integer
+ * Return: value of multiplication
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -19,5 +19,6 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif
