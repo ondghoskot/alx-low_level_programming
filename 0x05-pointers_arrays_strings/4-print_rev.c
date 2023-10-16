e1c23460f11234c3e1dd13751b7bddfd86d4c607
Anass <anaschtioui65@gmail.com>
2023-10-16 14:03:49 -0700

    task done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: given string
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
