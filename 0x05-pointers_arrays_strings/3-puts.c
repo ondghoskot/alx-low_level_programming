1deb7afb74f151e6cc9a4cde5092675e66f6241b
Anass <anaschtioui65@gmail.com>
2023-10-16 13:35:18 -0700

    task done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a given string
+ * @str: the given string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
