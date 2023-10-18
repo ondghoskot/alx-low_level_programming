880c97664fbcf3ac6ba74a2d9a4ae12be9f96fb7
Anass <anaschtioui65@gmail.com>
2023-10-18 12:41:03 +0000

    task done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings together 
+ * @dest: first string
+ * @src: appended string
+ * Return: concatenated strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int len1 = 0;
+	int len2 = 0;
+
+	while (dest[i] != '\0')
+	{
+		len1++;
+		i++;
+	};
+	while (src[j] != '\0')
+	{
+		len2++;
+		j++;
+	}
+	while (k < len2)
+	{
+		dest[len1 + k] = src[k];
+		k++;
+	}
+	return (dest);
+}
