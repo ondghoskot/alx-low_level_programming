ca1dfcc78b80843374530a006a15eeee43c9b2aa
Anass <anaschtioui65@gmail.com>
2023-10-19 10:40:46 +0000

    task done

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase letters in string to uppercase
+ * @str: lowercase string
+ * Return: pointer to string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	if (str == 0)
+	{
+		return (str);
+	}
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] -  ('a' - 'A');
+			i++;
+		}
+	}
+	return (str);
+}
