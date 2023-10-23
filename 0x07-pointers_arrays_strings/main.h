7e6293780fba69275864c476f041414857cd5776
Anass <anaschtioui65@gmail.com>
2023-10-23 17:48:03 +0000

    task done

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -50,5 +50,6 @@ char *leet(char *str);
 char *rot13(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
