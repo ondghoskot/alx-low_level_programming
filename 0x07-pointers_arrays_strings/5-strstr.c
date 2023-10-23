0ee2fd39a38408e2050d1060f6446ac63028bbb2
Anass <anaschtioui65@gmail.com>
2023-10-23 20:52:15 +0000

    task done

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the string to look in
+ * @needle: the string to look for
+ * Return: the intersection
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	while (haystack[i] != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack + i);
+		}
+		i++;
+	}
+	return (NULL);
+}
