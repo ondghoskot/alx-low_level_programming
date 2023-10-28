ff148d2635c1e162b6be81707e1094775da7836a
Anass <anaschtioui65@gmail.com>
2023-10-28 03:30:32 +0000

    task done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 		{
 			for (j = 0; argv[i][j]; j++)
 			{
-				if (argv[i][j] <= '0' || argv[i][j] >= '9')
+				if (argv[i][j] < '0' || argv[i][j] > '9')
 				{
 					printf("Error\n");
 					return (1);
