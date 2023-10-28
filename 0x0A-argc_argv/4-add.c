77302598ebb484e0fd69e7103a4a978271c4afa0
Anass <anaschtioui65@gmail.com>
2023-10-28 03:07:33 +0000

    task done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,7 +10,12 @@
 int main(int argc, char *argv[])
 {
 	int i, j, s = 0;
-
+	
+	if (argc == 1)
+	{
+		putchar('0');
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j]; j++)
