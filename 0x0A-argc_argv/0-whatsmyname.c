14a5919676dd83534f3252ff6618d24fc8a0abd8
Anass <anaschtioui65@gmail.com>
2023-10-28 01:57:16 +0000

    task done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file using argv
+ * @argc: number of CLA
+ * @argv: array of strings containing CLA
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
