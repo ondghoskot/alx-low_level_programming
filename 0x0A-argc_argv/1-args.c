1cb7caac3874f74495c5cc9ff5764ce0904c49c5
Anass <anaschtioui65@gmail.com>
2023-10-28 02:07:17 +0000

    task done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints number of CLA
+ * main - prints number of arguments passed into it
  * @argc: number of CLA
  * @argv: array of strings of CLA (unused arg in this case)
  * Return: 0 (success)
@@ -9,6 +9,6 @@
 int main(int argc, char *argv[])
 {
 	(void) argv;
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
