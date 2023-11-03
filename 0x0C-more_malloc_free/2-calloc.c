6d487b299020f8af5e77a7afe0be4eeccf75f8f4
Anass <anaschtioui65@gmail.com>
2023-11-03 18:21:23 +0000

    task done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,18 +8,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
 	unsigned int total_size = nmemb * size;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(total_size);
+	void *p = (void *)malloc(total_size);
+
 	if (p == NULL)
 		return (NULL);
 	char *char_ptr = (char *)p;
 
 	for (unsigned int i = 0; i < total_size; i++)
 		char_ptr[i] = 0;
-	return ((void *)p);
+	return (p);
 
 }
