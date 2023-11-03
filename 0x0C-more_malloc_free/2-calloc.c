0ed98c78b3d4c0cdeb581e307c05394e4c3001a6
Anass <anaschtioui65@gmail.com>
2023-11-03 14:24:24 +0000

    task done

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib>
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: number of elements in array
+ * @size: size of the element in bytes
+ * Return: pointer to allocatted memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int total_size = nmemb * size;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(total_size);
+	if (p == NULL)
+		return (NULL);
+	char *char_ptr = (char *)p;
+
+	for (unsigned int i = 0; i < total_size; i++)
+		char_ptr[i] = 0;
+	return (p);
+
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -71,5 +71,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
