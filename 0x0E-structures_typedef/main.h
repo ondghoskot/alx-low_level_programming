c6e495cd97f4da508e33e22d6c7f72c816a71878
Anass <anaschtioui65@gmail.com>
2023-11-06 21:13:03 +0000

    task done

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -64,5 +64,15 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
