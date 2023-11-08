eb03c0ce4d562137fc88efba3e1b63417ce604ad
Anass <anaschtioui65@gmail.com>
2023-11-08 21:43:23 +0000

    task done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,8 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		for (i = 0; i < size; i++)
 		{
-			cmp(array[i]);
-			return (i);
+			if(cmp(array[i]))
+				return (i);
 		}
 	}
 	return (-1);
