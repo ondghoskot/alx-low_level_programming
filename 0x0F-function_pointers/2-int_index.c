053e27c968b324f280901d712a3a85801727bbf3
Anass <anaschtioui65@gmail.com>
2023-11-08 21:41:39 +0000

    task done

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+/**
+ * int_index - searches for integer in array
+ * @array: give array to search
+ * @size: size of the array
+ * @cmp: function to compare an int
+ * Return: index of int or -1 if failed
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && size > 0 && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			cmp(array[i]);
+			return (i);
+		}
+	}
+	return (-1);
+}
