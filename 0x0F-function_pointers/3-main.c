df8bd39f8d0a229f02afb024f8ad1b56e871c4d6
Anass <anaschtioui65@gmail.com>
2023-11-08 23:06:50 +0000

    task done

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,20 +14,20 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (98);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if ((*argv[2] == '/' || *argv[2] == '%'))
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (100);
 	}
 	ptop = get_op_func(argv[2]);
 	if (ptop == NULL)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (98);
 	}
 	printf("%d\n", ptop(num1, num2));
