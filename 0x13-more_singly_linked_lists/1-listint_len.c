b281a22c3e105641ddc888cd811aca8b97fa1ef9
ondghoskot <anaschtioui65@gmail.com>
2023-11-21 23:02:26 +0000

    task done

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - calculates number of elements in a linked listint_t list
+ *@h: head of linked list
+ * Return: number of elements in a linked listint_t list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
