7ce9677e271bf78605207a5a070d507a48d8e7c7
ondghoskot <anaschtioui65@gmail.com>
2023-11-21 23:30:51 +0000

    task done

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,7 +18,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		end = *head;
 		while (end->next != NULL)
 			end = end->next;
-		end->next = new
+		end->next = new;
 	}
 	else
 		*head = new;
