f1261714996c449140a4499ff4870a5f84342a97
ondghoskot <anaschtioui65@gmail.com>
2023-11-21 18:22:34 +0000

    task done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,9 +9,10 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL && h = h->next)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
+		h = h->next;
 		count++;
 	}
 	return (count);
