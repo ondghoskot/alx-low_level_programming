7a23b658e497ede4b74c475ea45a17c38b53292e
ondghoskot <anaschtioui65@gmail.com>
2023-11-27 21:14:45 +0000

    task done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -39,7 +39,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	/* start write count */
-	w_count = write(STDOUT_FILENO, buffer, read_count);
+	w_count = write(STDOUT_FILENO, buffer, r_count);
 	free(buffer);
 	if (w_count < 0)
 		return (0);
