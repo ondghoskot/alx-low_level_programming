06962e9c052fa49133746d5ca00b59a239a75586
ondghoskot <anaschtioui65@gmail.com>
2023-11-27 21:29:00 +0000

    task done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file to create
+ * @text_content: string to write the file
+ * Return: 1 if success, -1 if failed
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t w = 0, fd;
+	size_t length = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONGLY | O_CREAT | 0_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[length] != '\0')
+		{
+			length++;
+		}
+		w = write(fd, text_content, length);
+	}
+	close(fd);
+	if (w == -1)
+		return (-1);
+	return (1);
+}
