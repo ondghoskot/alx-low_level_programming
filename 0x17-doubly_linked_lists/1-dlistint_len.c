41be346cf82f629b4052e60d4baddf749291f304
ondghoskot <anaschtioui65@gmail.com>
2024-01-11 23:26:11 +0000

    task done

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint - prints the number of nodes
+ * @h: ptr to the first node
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
