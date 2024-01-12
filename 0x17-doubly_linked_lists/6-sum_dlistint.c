70fdc1583ee28b17e4b15fa685beaf5792c94774
ondghoskot <anaschtioui65@gmail.com>
2024-01-12 19:12:59 +0000

    task done

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - adds up the data in each node in dll
+ * @head: ptr to first node in dll
+ * Retutn: sum od data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int s = 0;
+
+	while (head)
+	{
+		s += head->n;
+		head = head->next;
+	}
+	return (s);
+}
