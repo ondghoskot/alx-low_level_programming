d2046e4d7643c0fd83e17c0cee41b3d135f269dd
ondghoskot <anaschtioui65@gmail.com>
2024-01-12 18:41:57 +0000

    task done

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -21,7 +21,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		while(2nd_to_last_node->next != NULL)
 			2nd_to_last_node = 2nd_to_last_node->next;
 		2nd_to_last_node->next = new_node;
-		new_node->prev = 2nd_to_last_node
+		new_node->prev = 2nd_to_last_node;
 	}
 	else
 	{
