234b56b4ca8ef38e5ab30680584bb591e78f3c80
ondghoskot <anaschtioui65@gmail.com>
2024-03-29 12:48:46 +0000

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,2 +1,23 @@
 #usr/bin/python3
+""" Program of the island perimeter calculator"""
 
+def island_perimeter(grid):
+    """function that calculates the perimeter of an island
+    (cells of 1s inside a grid)"""
+    horizontal = 0
+    vertical = 0
+
+    #iterate through the outer list
+    for i in range (len(grid)):
+        #iterate through the inner lists
+        for j in range (len(grid[i])):
+            #checks if the first inner element is land
+            if grid[i][j]:
+                horizontal += 1
+                if horizontal < 2:
+                    vertical += 1
+                    if not grid[i][j+1]:
+                        horizontal = 0
+                        break
+    perimeter = (horizontal + vertical) * 2
+    return perimeter
