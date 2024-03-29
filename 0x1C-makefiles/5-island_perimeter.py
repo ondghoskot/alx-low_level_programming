4cd13fc99c588c9d281ce8d066f78bf6924ce049
ondghoskot <anaschtioui65@gmail.com>
2024-03-29 13:06:02 +0000

    changed the logic, proceeding with squares of perim 4

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 """ Program of the island perimeter calculator"""
 
+
 def island_perimeter(grid):
     """function that calculates the perimeter of an island
     (cells of 1s inside a grid)"""
-    horizontal = 0
-    vertical = 0
+    perimeter = 0
 
-    #iterate through the outer list
-    for i in range (len(grid)):
-        #iterate through the inner lists
-        for j in range (len(grid[i])):
-            #checks if the first inner element is land
+    # iterate through the outer list
+    for i in range(len(grid)):
+        # iterate through the inner lists
+        for j in range(len(grid[i])):
+            # checks if the first inner element is land
             if grid[i][j]:
-                horizontal += 1
-                if horizontal < 2:
-                    vertical += 1
-                    if not grid[i][j+1]:
-                        horizontal = 0
-                        break
-    perimeter = (horizontal + vertical) * 2
+                perimeter += 4
+                if grid[i - 1][j] and i > 0:
+                    perimeter -= 2
+                if grid[i][j - 1] and j > 0:
+                    perimeter -= 2
     return perimeter
-
