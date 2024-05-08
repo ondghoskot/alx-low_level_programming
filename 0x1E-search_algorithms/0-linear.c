a10a244729559f7346c3a8d5af36853696e56d88
anassch <anaschtioui65@gmail.com>
2024-05-08 23:46:13 +0000

    linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - performs a linear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index of valuei if found, otherwise -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i, index;
+
+	index = -1;
+	if (array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] == value)
+			{
+				index = i;
+				return index;
+			}
+		}
+	}
+	return index;
+}
